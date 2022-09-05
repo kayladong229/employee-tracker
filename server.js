f76010a23455509a544e201ff2dd9dca9b55499f
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-05 13:41:36 -0700

    Added code to view roles, view names and add employees

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
 // Connect to database
 const db = mysql.createConnection({
     host: 'localhost',
@@ -27,7 +20,7 @@ db.connect((err) => {
   if(err) {
     console.log('An error has occurred.')
   }
-  console.log('\nSuccessfully connected to the employees_db database under ID ' + db.threadId + '.');
+  console.log('\nSuccessfully connected to the employees_db database under ID ' + db.threadId + '.\n');
   startApp();
 });
 
@@ -38,21 +31,100 @@ const startApp = () => {
         type: 'list',
         name: 'choice',
         message: 'What would you like to do? Press CTRL + C to exit this application.',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add A Role', 'View All Departments', 'Add A Department']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update Employee Role']
       },
-  ])
-  .then ((answer) => {
-    switch(answer.choice) {
-      case 'View All Employees':
-        viewEmployees();
-        break;
-    }
+    ])
+    .then ((answer) => {
+      switch(answer.choice) {
+        case 'View All Departments':
+          viewDepartments();
+          break;
+        case 'View All Roles':
+          viewRoles();
+          break;
+        case 'View All Employees':
+          viewEmployees();
+          break;
+        case 'Add A Department':
+          addDepartment();
+          break;
+        case 'Add An Employee':
+          addEmployee();
+          break;
+      }
+    })
+};
+
+// Function to view all departments
+const viewDepartments = () => {
+  db.query("SELECT * FROM department", function(err, results) {
+    console.table('\nAll departments: ', results);
+    startApp();
   })
 }
-// Function to view all employees
+
+// Function to view all roles
+const viewRoles = () => {
+  db.query("SELECT * FROM role", function(err, results) {
+    console.table('\nAll roles: ', results);
+    startApp();
+  })
+}
+
+// // Function to view all employees
 const viewEmployees = () => {
   db.query("SELECT * FROM employee", function(err, results) {
-    console.log(results);
+    console.table('\nAll employees: ', results);
     startApp();
   })
 };
+
+// // Function to add a department
+const addDepartment = () => {
+  return inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: "What is the name of your new department?"
+    }
+  ])
+  .then (answer => {
+    db.query('INSERT INTO department (name) VALUES (?)', answer.departmentName, (err, result) => {
+      if (err) {
+        console.log("An error has occurred.");
+        return;
+      }
+    console.log(`\nSuccessfully added ${answer.departmentName} to the departments table.\n`);
+    // Log updated department table to console
+    viewDepartments();
+    })
+  })
+  startApp();
+};
+
+// // Function to add an employee
+// const addEmployee = () => {
+//   return inquirer
+//   .prompt([
+//     {
+//       type: 'text',
+//       name: 'firstname',
+//       message: "What is this employee's first name?"
+//     }
+//     {
+//       type: 'text',
+//       name: 'lastname',,
+//       message: "What is this employee's last name?"
+//     }
+//     {
+//       type: 'text',
+//       name: 'role',
+//       message: "What is this employee's role?"
+//     }
+//     {
+//       type: 'text',
+//       name: ''
+//     }
+//   ])
+// }
