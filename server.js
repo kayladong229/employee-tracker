a31e0ef1cf6f552590fd7d3e2e6ea9b140ee13b9
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-05 14:38:31 -0700

    Added code to create new roles

--- server.js
+++ server.js
@@ -48,6 +48,9 @@ const startApp = () => {
         case 'Add A Department':
           addDepartment();
           break;
+        case 'Add A Role':
+          addRole();
+          break;
         case 'Add An Employee':
           addEmployee();
           break;
@@ -79,7 +82,7 @@ const viewEmployees = () => {
   })
 };
 
-// // Function to add a department
+// Function to add a department
 const addDepartment = () => {
   return inquirer
   .prompt([
@@ -101,6 +104,62 @@ const addDepartment = () => {
     })
   })
   startApp();
+};
+
+// // Function to add a role
+const addRole = () => {
+  db.query('SELECT * FROM department', (err, result) => {
+    if (err) {
+      console.log("An error has occurred.");
+      return;
+    }
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'roleName',
+          message: "What is the name of your new role?"
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: "What is the annual salary for this role?"
+        },
+        {
+          type: 'list',
+          name: 'department',
+          message: "What department is this role under?",
+          choices: function() {
+            let departmentArray = [];
+            for (let i = 0; i < result.length; i++) {
+            departmentArray.push(result[i].name);
+          }
+          return departmentArray;
+          }
+        }
+    ])
+    .then (answer => {
+      let department_id;
+      for (let j = 0; j < result.length; j++) {
+        if (result[j].name == answer.department) {
+          department_id = result[j].id;
+        }
+      }
+  
+      db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', 
+        [answer.roleName, answer.salary, department_id], 
+        (err, result) => {
+          if (err) {
+            console.log("An error has occurred.");
+            return;
+          }
+        console.log(`\nSuccessfully added ${answer.roleName} to the role table.\n`);
+        // Log updated role table to console
+        viewRoles();
+        startApp();
+        })
+      })
+    })
 };
 
 // // Function to add an employee
